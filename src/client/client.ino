9ee89b3df62e0b3919c18ee59c61c77d4fb27b11
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-15 12:18:01 +0100

    Push Update 15/02/2025, 12:18:01

--- src/client/client.ino
+++ src/client/client.ino
@@ -7,15 +7,23 @@
 
 
 
+
+static unsigned char _packet[1];
+
+
+
 void setup(void){
+	Serial.begin(115200);
 	pinMode(LED_BUILTIN,OUTPUT);
 }
 
 
 
 void loop(void){
-	digitalWrite(LED_BUILTIN,1);
-	delay(200);
-	digitalWrite(LED_BUILTIN,0);
-	delay(200);
+	if (Serial.available()>0){
+		if (Serial.readBytes(_packet,sizeof(_packet))<sizeof(_packet)){
+			return;
+		}
+		digitalWrite(LED_BUILTIN,_packet[0]&1);
+	}
 }
