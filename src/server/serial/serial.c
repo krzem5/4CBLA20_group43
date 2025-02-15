59739d9901a67ec98db32034145a6b6bab5dfe1a
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-15 16:45:46 +0100

    Push Update 15/02/2025, 16:45:46

--- src/server/serial/serial.c
+++ src/server/serial/serial.c
@@ -7,6 +7,7 @@
 
 
 
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <serial/serial.h>
@@ -21,18 +22,33 @@ static int _serial_fd=-1;
 
 
 void serial_init(void){
-	_serial_fd=open(SERIAL_DEVICE,O_RDWR|O_NOCTTY|O_SYNC);
-	ASSERT(_serial_fd>=0||!"Serial device not found");
-	struct termios tty;
-	ASSERT(!tcgetattr(_serial_fd,&tty));
-	tty.c_iflag&=~(IGNBRK|IXON|IXANY|IXOFF);
-	tty.c_oflag=0;
-	tty.c_cflag=(tty.c_cflag&(~(CSIZE|CSTOPB|PARENB|PARODD|CRTSCTS)))|CS8|CREAD|CLOCAL|SERIAL_PARITY;
-	tty.c_lflag=0;
-	tty.c_cc[VMIN]=0;
-	tty.c_cc[VTIME]=0;
-	ASSERT(!cfsetospeed(&tty,SERIAL_SPEED)&&!cfsetispeed(&tty,SERIAL_SPEED));
-	ASSERT(!tcsetattr(_serial_fd,TCSANOW,&tty));
+	DIR* dir=opendir("/dev/serial/by-id/");
+	for (struct dirent* entry=readdir(dir);entry;entry=readdir(dir)){
+		if (entry->d_type!=DT_LNK){
+			continue;
+		}
+		_serial_fd=openat(dirfd(dir),entry->d_name,O_RDWR|O_NOCTTY|O_SYNC);
+		if (_serial_fd<0){
+			continue;
+		}
+		struct termios tty;
+		if (tcgetattr(_serial_fd,&tty)){
+			close(_serial_fd);
+			continue;
+		}
+		closedir(dir);
+		tty.c_iflag&=~(IGNBRK|IXON|IXANY|IXOFF);
+		tty.c_oflag=0;
+		tty.c_cflag=(tty.c_cflag&(~(CSIZE|CSTOPB|PARENB|PARODD|CRTSCTS)))|CS8|CREAD|CLOCAL|SERIAL_PARITY;
+		tty.c_lflag=0;
+		tty.c_cc[VMIN]=0;
+		tty.c_cc[VTIME]=0;
+		ASSERT(!cfsetospeed(&tty,SERIAL_SPEED)&&!cfsetispeed(&tty,SERIAL_SPEED));
+		ASSERT(!tcsetattr(_serial_fd,TCSANOW,&tty));
+		return;
+	}
+	closedir(dir);
+	ASSERT(!"No serial device found");
 }
 
 
