13fbbe620c6e404b24202152350c6ef202125b0b
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-16 18:12:43 +0100

    Push Update 16/02/2025, 18:12:43

--- src/client/gpio/gpio.c
+++ src/client/gpio/gpio.c
@@ -13,69 +13,18 @@
 
 
 
-const ROM_DECL uint16_t _gpio_to_mode_reg[]={
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRD),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRB),
-	(uint16_t)(&DDRC),
-	(uint16_t)(&DDRC),
-	(uint16_t)(&DDRC),
-	(uint16_t)(&DDRC),
-	(uint16_t)(&DDRC),
-	(uint16_t)(&DDRC)
+const ROM_DECL uint16_t _gpio_pin_to_mode_reg[]={
+	(uint16_t)(&DDRD),(uint16_t)(&DDRD),(uint16_t)(&DDRD),(uint16_t)(&DDRD),
+	(uint16_t)(&DDRB),(uint16_t)(&DDRB),(uint16_t)(&DDRB),
+	(uint16_t)(&DDRC),(uint16_t)(&DDRC),(uint16_t)(&DDRC)
 };
-const ROM_DECL uint16_t _gpio_to_output_reg[]={
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTD),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTB),
-	(uint16_t)(&PORTC),
-	(uint16_t)(&PORTC),
-	(uint16_t)(&PORTC),
-	(uint16_t)(&PORTC),
-	(uint16_t)(&PORTC),
-	(uint16_t)(&PORTC)
+const ROM_DECL uint16_t _gpio_pin_to_output_reg[]={
+	(uint16_t)(&PORTD),(uint16_t)(&PORTD),(uint16_t)(&PORTD),(uint16_t)(&PORTD),
+	(uint16_t)(&PORTB),(uint16_t)(&PORTB),(uint16_t)(&PORTB),
+	(uint16_t)(&PORTC),(uint16_t)(&PORTC),(uint16_t)(&PORTC)
 };
-const ROM_DECL uint8_t _gpio_to_mask[]={
-	0x01,
-	0x02,
-	0x04,
-	0x08,
-	0x10,
-	0x20,
-	0x40,
-	0x80,
-	0x01,
-	0x02,
-	0x04,
-	0x08,
-	0x10,
-	0x20,
-	0x01,
-	0x02,
-	0x04,
-	0x08,
-	0x10,
-	0x20,
+const ROM_DECL uint8_t _gpio_pin_to_mask[]={
+	0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,
+	0x01,0x02,0x04,0x08,0x10,0x20,
+	0x01,0x02,0x04,0x08,0x10,0x20,
 };
--- src/client/include/gpio/gpio.h
+++ src/client/include/gpio/gpio.h
@@ -17,29 +17,29 @@ extern "C" {
 
 
 
-extern const ROM_DECL uint16_t _gpio_to_mode_reg[];
-extern const ROM_DECL uint16_t _gpio_to_output_reg[];
-extern const ROM_DECL uint8_t _gpio_to_mask[];
+extern const ROM_DECL uint16_t _gpio_pin_to_mode_reg[];
+extern const ROM_DECL uint16_t _gpio_pin_to_output_reg[];
+extern const ROM_DECL uint8_t _gpio_pin_to_mask[];
 
 
 
-static inline volatile uint8_t* _gpio_load_port(const uint16_t* ptr){
-	return (volatile uint8_t*)(void*)ROM_LOAD_U16(ptr);
+static inline volatile uint8_t* _gpio_load_port(const void* base,uint8_t pin){
+	return (volatile uint8_t*)(void*)ROM_LOAD_U16(base+(pin&0xfe));
 }
 
 
 
 static inline void gpio_init(uint8_t pin){
-	uint8_t mask=ROM_LOAD_U8(_gpio_to_mask+pin);
-	(*_gpio_load_port(_gpio_to_output_reg+pin))&=~mask;
-	(*_gpio_load_port(_gpio_to_mode_reg+pin))|=mask;
+	uint8_t mask=ROM_LOAD_U8(_gpio_pin_to_mask+pin);
+	(*_gpio_load_port(_gpio_pin_to_output_reg,pin))&=~mask;
+	(*_gpio_load_port(_gpio_pin_to_mode_reg,pin))|=mask;
 }
 
 
 
 static inline void gpio_write(uint8_t pin,_Bool state){
-	uint8_t mask=ROM_LOAD_U8(_gpio_to_mask+pin);
-	volatile uint8_t* port=_gpio_load_port(_gpio_to_output_reg+pin);
+	uint8_t mask=ROM_LOAD_U8(_gpio_pin_to_mask+pin);
+	volatile uint8_t* port=_gpio_load_port(_gpio_pin_to_output_reg,pin);
 	if (state){
 		(*port)|=mask;
 	}
