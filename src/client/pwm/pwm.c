2e11e837a3db4f26b14caa8cddc4d89cbc497af3
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-16 11:04:03 +0100

    Push Update 16/02/2025, 11:04:03

--- src/client/pwm/pwm.c
+++ src/client/pwm/pwm.c
@@ -15,8 +15,8 @@
 #define PWM_TICKS_PER_US_SHIFT (__builtin_ffs(F_CPU/8000000)-1)
 
 #define PWM_MIN_PERIOD_US 20000
-#define PWM_MIN_PULSE_US 2
-#define PWM_MAX_PULSE_US (0xffff>>PWM_PIN_BIT_COUNT)
+#define PWM_MIN_PULSE_US 4
+#define PWM_MAX_PULSE_US ((1<<((sizeof(_pwm_data[0])<<3)-PWM_PIN_BIT_COUNT))-1)
 
 #define PWM_MAX_PIN_COUNT 8
 #define PWM_PIN_BIT_COUNT 4
@@ -24,31 +24,42 @@
 
 
 static volatile uint16_t __attribute__((section(".bss"))) _pwm_data[PWM_MAX_PIN_COUNT+1];
-static pwm_t _pwm_entry_index=0;
+static pwm_t _pwm_rr_scheduler_entry_index=0;
+
+
+
+static inline void _fast_digital_pin_write(uint8_t pin,_Bool high){
+	if (high){
+		(*portOutputRegister(digitalPinToPort(pin)))|=digitalPinToBitMask(pin);
+	}
+	else{
+		(*portOutputRegister(digitalPinToPort(pin)))&=~digitalPinToBitMask(pin);
+	}
+}
 
 
 
 SIGNAL(TIMER1_COMPA_vect){
-	uint16_t entry=_pwm_data[_pwm_entry_index];
+	uint16_t entry=_pwm_data[_pwm_rr_scheduler_entry_index];
 	if (entry){
-		digitalWrite(entry&((1<<PWM_PIN_BIT_COUNT)-1),0);
-		_pwm_entry_index++;
+		_fast_digital_pin_write(entry&((1<<PWM_PIN_BIT_COUNT)-1),0);
+		_pwm_rr_scheduler_entry_index++;
 	}
 	else{
-_next_pin:
+_reset_rr_scheduler:
 		TCNT1=0;
-		_pwm_entry_index=0;
+		_pwm_rr_scheduler_entry_index=0;
 	}
-	entry=_pwm_data[_pwm_entry_index];
+	entry=_pwm_data[_pwm_rr_scheduler_entry_index];
 	if (entry){
-		digitalWrite(entry&((1<<PWM_PIN_BIT_COUNT)-1),1);
+		_fast_digital_pin_write(entry&((1<<PWM_PIN_BIT_COUNT)-1),1);
 		OCR1A=TCNT1+((entry>>4)<<PWM_TICKS_PER_US_SHIFT);
 	}
 	else if (TCNT1<(PWM_MIN_PERIOD_US<<PWM_TICKS_PER_US_SHIFT)){
 		OCR1A=(PWM_MIN_PERIOD_US+PWM_MIN_PULSE_US)<<PWM_TICKS_PER_US_SHIFT;
 	}
 	else{
-		goto _next_pin;
+		goto _reset_rr_scheduler;
 	}
 }
 
@@ -56,9 +67,9 @@ _next_pin:
 
 void pwm_init(void){
 	TCCR1A=0;
-	TCCR1B=1<<CS11; // divide by 8
+	TCCR1B=1<<CS11; // divide by 8 (see p110 of the datasheet)
 	TCNT1=0;
-	TIFR1=1<<OCF1A;
+	TIFR1=(1<<TOV1)|(1<<OCF1A)|(1<<OCF1B)|(1<<ICF1);
 	OCR1A=PWM_MIN_PERIOD_US;
 	TIMSK1=1<<OCIE1A;
 }
@@ -80,5 +91,9 @@ pwm_t pwm_alloc(uint8_t pin){
 
 
 void pwm_set_pulse_width_us(pwm_t index,uint16_t us){
-	_pwm_data[index]=(_pwm_data[index]&((1<<PWM_PIN_BIT_COUNT)-1))|((us<PWM_MIN_PULSE_US?PWM_MIN_PULSE_US:(us>PWM_MAX_PULSE_US?PWM_MAX_PULSE_US:us))<<PWM_PIN_BIT_COUNT);
+	uint16_t value=(_pwm_data[index]&((1<<PWM_PIN_BIT_COUNT)-1))|((us<PWM_MIN_PULSE_US?PWM_MIN_PULSE_US:(us>PWM_MAX_PULSE_US?PWM_MAX_PULSE_US:us))<<PWM_PIN_BIT_COUNT);
+	uint8_t status_reg_value=SREG;
+	cli();
+	_pwm_data[index]=value;
+	SREG=status_reg_value;
 }
