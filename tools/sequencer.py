a2dbe6199a57b17e52ec1a11025e95d0d9027cad
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-17 17:03:20 +0100

    Push Update 17/02/2025, 17:03:20

--- tools/sequencer.py
+++ tools/sequencer.py
@@ -40,27 +40,27 @@ def _generate_sequencer_header(dst_file_path,data):
 			sequencer_data.append((PWM_SEQUENCER_PIN_FLAG_INVERTED|(~k) if k<0 else k)+(PWM_SEQUENCER_PIN_FLAG_LAST if i==len(channel["pins"])-1 else 0))
 		for k in channel["points"]:
 			last_time=max(last_time,k[0])
-	sample_delta=TIMER_TICKS*TIMER_DIVISOR*TIMER_VIRTUAL_DIVISOR/CPU_FREQ*1000
+	sample_delta=TIMER_TICKS*TIMER_DIVISOR*TIMER_VIRTUAL_DIVISOR/CPU_FREQ
 	sample_count=math.ceil(last_time/sample_delta)+1
 	sequencer_data[1]=sample_count&0xff
 	sequencer_data[2]=sample_count>>8
-	last_channel_values=[-1 for _ in range(0,channel_count)]
-	last_channel_offsets=[0 for _ in range(0,channel_count)]
+	last_channel_value=[-1 for _ in range(0,channel_count)]
+	last_channel_offset=[0 for _ in range(0,channel_count)]
 	last_channel_repeat_byte_index=[0 for _ in range(0,channel_count)]
 	for i in range(0,sample_count):
 		t=i*sample_delta
 		for j in range(0,channel_count):
 			points=data[j]["points"]
-			if (last_channel_offsets[j]+1<len(points) and points[last_channel_offsets[j]+1][0]<=t):
-				last_channel_offsets[j]+=1
-			if (last_channel_offsets[j]+1>=len(points)):
-				angle=points[last_channel_offsets[j]][1]
+			if (last_channel_offset[j]+1<len(points) and points[last_channel_offset[j]+1][0]<=t):
+				last_channel_offset[j]+=1
+			if (last_channel_offset[j]+1>=len(points)):
+				angle=points[last_channel_offset[j]][1]
 			else:
-				a,b=points[last_channel_offsets[j]],points[last_channel_offsets[j]+1]
+				a,b=points[last_channel_offset[j]],points[last_channel_offset[j]+1]
 				angle=(0.5-math.cos((t-a[0])/(b[0]-a[0])*math.pi)/2)*(b[1]-a[1])+a[1]
 			encoded_pulse=ANGLE_TO_ENCODED_PULSE(angle)
-			if (last_channel_values[j]!=encoded_pulse):
-				last_channel_values[j]=encoded_pulse
+			if (last_channel_value[j]!=encoded_pulse):
+				last_channel_value[j]=encoded_pulse
 				last_channel_repeat_byte_index[j]=0
 				sequencer_data.append(encoded_pulse)
 				continue
