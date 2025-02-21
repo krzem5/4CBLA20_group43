3e3516e8d5306882f843bca731793c24b15a0059
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-21 19:28:28 +0100

    Push Update 21/02/2025, 19:28:28

--- graphs/kinematics.py
+++ graphs/kinematics.py
@@ -31,62 +31,72 @@ DELTA_TIME=0.001
 
 
 
-plt.rcParams["text.usetex"]=True
-fig,ax=plt.subplots(1)
 end_time=0
 with open(FILE_PATH,"r") as rf:
 	data=json.loads(rf.read())
-	for index,channel in enumerate(data):
-		points=channel["points"]
-		x=[]
-		y=[]
-		for i in range(0,len(points)-1):
-			a,b=points[i:i+2]
-			t=a[0]
-			while (t<b[0]):
-				x.append(t)
-				y.append((0.5-math.cos((t-a[0])/(b[0]-a[0])*math.pi)/2)*(b[1]-a[1])+a[1])
-				t+=DELTA_TIME
-			end_time=max(end_time,t)
-		x.append(points[-1][0])
-		y.append(points[-1][1])
-		ax.plot(x,y,"--",color=CHANNELS[index][0],alpha=0.5,linewidth=5)
-	size_float32=(math.ceil(end_time/sequencer_compiler.SAMPLE_DELTA)+1)*4*len(data)
-	size_uint8=(math.ceil(end_time/sequencer_compiler.SAMPLE_DELTA)+1)*len(data)
-	data=sequencer_compiler.compile_sequence(data)
-	size_compressed=len(data)
-	channel_count=data[0]&7
-	sample_count=data[1]|((data[0]>>3)<<8)
-	state=[None for _ in range(0,channel_count*3)]
-	x=[]
-	y=[[] for _ in range(0,channel_count)]
-	idx=channel_count+2
-	for i in range(0,channel_count*3,3):
-		state[i]=0
-		state[i+1]=0
-		state[i+2]=data[idx]
-		idx+=1
-	for i in range(0,sample_count):
-		x.append(i*sequencer_compiler.SAMPLE_DELTA)
-		for j in range(0,channel_count*3,3):
-			if (state[j]):
-				state[j]-=1
+theoretical_y=[[] for _ in range(0,len(data))]
+for i,channel in enumerate(data):
+	points=channel["points"]
+	t=points[0][0]
+	for j in range(0,len(points)-1):
+		a,b=points[j:j+2]
+		while (t<b[0]):
+			theoretical_y[i].append((0.5-math.cos((t-a[0])/(b[0]-a[0])*math.pi)/2)*(b[1]-a[1])+a[1])
+			t+=DELTA_TIME
+		end_time=max(end_time,t)
+	theoretical_y[i].append(points[-1][1])
+theoretical_x=[i*DELTA_TIME for i in range(0,math.ceil(end_time/DELTA_TIME)+1)]
+size_uncompressed=(math.ceil(end_time/sequencer_compiler.SAMPLE_DELTA)+1)*4*len(data)
+data=sequencer_compiler.compile_sequence(data)
+size_compressed=len(data)
+channel_count=data[0]&7
+sample_count=data[1]|((data[0]>>3)<<8)
+state=[None for _ in range(0,channel_count*3)]
+x=[]
+y=[[] for _ in range(0,channel_count)]
+idx=channel_count+2
+for i in range(0,channel_count*3,3):
+	state[i]=0
+	state[i+1]=0
+	state[i+2]=data[idx]
+	idx+=1
+for i in range(0,sample_count):
+	x.append(i*sequencer_compiler.SAMPLE_DELTA)
+	for j in range(0,channel_count*3,3):
+		if (state[j]):
+			state[j]-=1
+		else:
+			token=data[idx]
+			idx+=1
+			if (token&1):
+				state[j]=token>>6
+				state[j+1]=((token>>2)&15)+1
+				if (token&2):
+					state[j+1]=-state[j+1]
 			else:
-				token=data[idx]
-				idx+=1
-				if (token&1):
-					state[j]=token>>6
-					state[j+1]=((token>>2)&15)+1
-					if (token&2):
-						state[j+1]=-state[j+1]
-				else:
-					state[j]=token>>1
-					state[j+1]=0
-			state[j+2]+=state[j+1]
-			y[j//3].append(sequencer_compiler.ENCODED_PULSE_TO_ANGLE(state[j+2]))
-	for i in range(0,channel_count):
-		ax.plot(x,y[i],"-",color=CHANNELS[i][0],label=CHANNELS[i][1])
-	print(f"compressed: {size_compressed}, uint8: {size_uint8}, float32: {size_float32}\nw.r.t. uint8: {size_compressed/size_uint8-1:.1%}, w.r.t. float32 {size_compressed/size_float32-1:.1%}")
+				state[j]=token>>1
+				state[j+1]=0
+		state[j+2]+=state[j+1]
+		y[j//3].append(sequencer_compiler.ENCODED_PULSE_TO_ANGLE(state[j+2]))
+plt.rcParams["text.usetex"]=True
+fig,ax=plt.subplots(1)
+for i in range(0,channel_count):
+	ax.plot(theoretical_x,theoretical_y[i],"--",color=CHANNELS[i][0],alpha=0.5,linewidth=5)
+	avg_err=0
+	max_err=0
+	j=0
+	for k in range(0,len(theoretical_x)):
+		if (j+1<len(x) and theoretical_x[k]>=x[j+1]):
+			j+=1
+		if (j+1==len(x)):
+			break
+		err=abs((theoretical_x[k]-x[j])/(x[j+1]-x[j])*(y[i][j+1]-y[i][j])+y[i][j]-theoretical_y[i][k])
+		avg_err+=err
+		max_err=max(max_err,err)
+	print(f"[{i}] avg={avg_err/(end_time/DELTA_TIME):.3f} deg, max={max_err:.3f} deg")
+for i in range(0,channel_count):
+	ax.plot(x,y[i],"-",color=CHANNELS[i][0],label=CHANNELS[i][1])
+print(f"compressed: {size_compressed}, uncompressed: {size_uncompressed} => {size_compressed/size_uncompressed-1:.1%}")
 fig.set_size_inches(13,4,forward=True)
 plt.xlabel("Time $\\left[\\mathrm{s}\\right]$")
 plt.ylabel("Angle $\\left[\\mathrm{deg}\\right]$")
