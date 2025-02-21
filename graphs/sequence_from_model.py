a9eac37e9de94c45057ebfaa40ac0d402c17ee5a
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-02-21 12:21:21 +0100

    Push Update 21/02/2025, 12:21:21

--- graphs/sequence_from_model.py
+++ graphs/sequence_from_model.py
@@ -15,7 +15,7 @@ import math
 FILE_PATH="../data/sequence.json"
 
 SERVIO_MAX_ANGULAR_VELOCITY=360 # deg/s
-SERVIO_ACCELERATION_PROFILE_MARGIN=0.2
+SERVIO_ACCELERATION_PROFILE_MARGIN=0.4
 
 WHEEL_RADIUS=0.45 # dm
 
@@ -65,14 +65,27 @@ FINAL_LINKAGE_MOTION=[
 
 
 
+
+def _encode_delta(delta,out):
+	profile=SERVIO_ACCELERATION_PROFILE_MARGIN
+	value=90+delta/(1-profile)/SERVIO_MAX_ANGULAR_VELOCITY*90
+	if (value<0):
+		profile=1+delta/SERVIO_MAX_ANGULAR_VELOCITY
+		value=0
+	elif (value>180):
+		profile=1-delta/SERVIO_MAX_ANGULAR_VELOCITY
+		value=180
+	t=out[-1][0]+1
+	out.append([t-1+profile,value])
+	out.append([t-profile,value])
+	out.append([t,90])
+
+
+
 def _compute_wheel_channel():
 	points=[[0,90]]
 	for i in range(1,len(WHEEL_MOTION)):
-		delta=WHEEL_MOTION[i]/(2*math.pi*WHEEL_RADIUS)*360
-		value=90+delta/(1-SERVIO_ACCELERATION_PROFILE_MARGIN)/SERVIO_MAX_ANGULAR_VELOCITY*90
-		points.append([i-1+SERVIO_ACCELERATION_PROFILE_MARGIN,value])
-		points.append([i-SERVIO_ACCELERATION_PROFILE_MARGIN,value])
-		points.append([i,90])
+		_encode_delta(WHEEL_MOTION[i]/(2*math.pi*WHEEL_RADIUS)*360,points)
 	return {"pin_a":LINKAGE_PINS[0][0],"pin_b":LINKAGE_PINS[0][1],"points":points}
 
 
@@ -80,11 +93,7 @@ def _compute_wheel_channel():
 def _compute_middle_linkage_channel():
 	points=[[0,90]]
 	for i in range(1,len(MIDDLE_LINKAGE_MOTION)):
-		delta=MIDDLE_LINKAGE_MOTION[i]-MIDDLE_LINKAGE_MOTION[i-1]
-		value=90+delta/(1-SERVIO_ACCELERATION_PROFILE_MARGIN)/SERVIO_MAX_ANGULAR_VELOCITY*90
-		points.append([i-1+SERVIO_ACCELERATION_PROFILE_MARGIN,value])
-		points.append([i-SERVIO_ACCELERATION_PROFILE_MARGIN,value])
-		points.append([i,90])
+		_encode_delta(MIDDLE_LINKAGE_MOTION[i]-MIDDLE_LINKAGE_MOTION[i-1],points)
 	return {"pin_a":LINKAGE_PINS[1][0],"pin_b":LINKAGE_PINS[1][1],"points":points}
 
 
